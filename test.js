c100dcdc383519ca3e9b19f6c943d28b807dd30d
amaralfelipe1522 <amaral.felipe@live.com>
2020-05-25 09:59:22 -0300

    Class 0203 completed

---
+++ test.js
@@ -0,0 +1,9 @@
+function getData() {
+    let url = "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_c7e6973d3efc4779a14a16f35d91edd2";
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+    })
+}
+getData();
